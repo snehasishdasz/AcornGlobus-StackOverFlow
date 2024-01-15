716f9561bd713c8f3e4fe40ddea45b2acdf999ae
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2024-01-16 01:51:57 +0530

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
+import {BrowserRouter as Router , Route, Routes} from "react-router-dom";
 import './App.css';
 import Header from './components/Header/Header.jsx';
 
 function App() {
   return (
     <>
+    <Router>
       <Header/>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+
+    </Router>
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,25 @@
-
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import "./Header.css"
 const Header = () => {
     
     return (
       <>
-        header
+        <nav>
+          <div className="navbar">
+            <Link to="/" className="nav-item nav-logo">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Stack_Overflow_logo.svg/220px-Stack_Overflow_logo.svg.png" alt="logo" />
+            </Link>
+            <form >
+              <FontAwesomeIcon icon={faMagnifyingGlass} width="18" className="search-icon"/>
+              <input type="text" placeholder="Search..."/>
+            </form>
+            <Link to="/" className="nav-item nav-btn">About</Link>
+            <Link to="/" className="nav-item nav-btn">Product</Link>
+            <Link to="/" className="nav-item nav-btn">For Teams</Link>
+          </div>
+        </nav>
       </>
     );
 }
