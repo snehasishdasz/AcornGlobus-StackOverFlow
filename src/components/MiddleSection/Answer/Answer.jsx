ba029f30c836117a3cd39e0913f7ca88fda74e32
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2024-01-17 03:06:06 +0530

    Creating Question card and answer Card for the middle part

---
+++ src/components/MiddleSection/Answer/Answer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Answer.scss"
+const Answer = () => {
+  return (
+    <div>Answer</div>
+  )
+}
+
+export default Answer
---
+++ src/components/MiddleSection/Question/Question.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Question.scss"
+const Question = () => {
+  return (
+    <div>Question</div>
+  )
+}
+
+export default Question
