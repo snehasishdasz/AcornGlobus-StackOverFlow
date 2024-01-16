fea523541c011c6f9e3ba724572d8e566104f8ac
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2024-01-17 02:21:33 +0530

    navbar ui done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import {BrowserRouter as Router , Route, Routes} from "react-router-dom";
 import './App.css';
 import Header from './components/Header/Header.jsx';
+import LeftSideBar from "./components/LeftSideBar/LeftSideBar.jsx";
 
 function App() {
   return (
     <>
     <Router>
       <Header/>
+      <LeftSideBar/>
       <Routes>
         <Route path="/" />
       </Routes>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,30 @@
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
-import "./Header.css"
+import { AiOutlineSearch, AiOutlineMenu } from "react-icons/ai";
+import "./Header.scss"
+import Navlogo from "../../assets/navlogo.svg"
 const Header = () => {
     
     return (
       <>
-        <nav>
-          <div className="navbar">
-            <Link to="/" className="nav-item nav-logo">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Stack_Overflow_logo.svg/220px-Stack_Overflow_logo.svg.png" alt="logo" />
-            </Link>
-            <form >
-              <FontAwesomeIcon icon={faMagnifyingGlass} width="18" className="search-icon"/>
-              <input type="text" placeholder="Search..."/>
-            </form>
-            <Link to="/" className="nav-item nav-btn">About</Link>
-            <Link to="/" className="nav-item nav-btn">Product</Link>
-            <Link to="/" className="nav-item nav-btn">For Teams</Link>
-          </div>
-        </nav>
+        <div className="navbar">
+          <section className="navbar_wrapper">
+            <div className="navbar">
+              <div className="logo">
+                <img src={Navlogo} alt="stack-over-flow-logo" />
+              </div>
+              <div className="search_wrapper">
+              <AiOutlineSearch className="search_icon" size={20} />
+              <input type="text" className="search_input" placeholder="Search" />
+              </div>
+              <div className="menu">
+                <a href="/">About</a>
+                <a href="/">Products</a>
+                <a href="/">For teams</a>
+              </div>
+              <AiOutlineMenu className="menu_bar" size={20}/>
+            </div>
+          </section>
+        </div>
       </>
     );
 }
