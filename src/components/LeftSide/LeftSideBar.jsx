aea6e45ac222e0b251be9b2203d94b05309d4cb9
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2024-01-17 19:19:08 +0530

    Left Sidebar UI modification

--- src/components/LeftSide/LeftSideBar.jsx
+++ src/components/LeftSide/LeftSideBar.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import { AiOutlineHome, AiOutlineGlobal, AiOutlineTeam } from "react-icons/ai";
+import React, { useState } from 'react'
+import { AiOutlineHome, AiOutlineGlobal, AiOutlineTeam, AiOutlineUp, AiOutlineDown } from "react-icons/ai";
 import { HiOutlineBriefcase } from "react-icons/hi2";
 import "./LeftSideBar.scss"
 const LeftSideBar = () => {
+  const [showSubMenu, setShowSubMenu] = useState(false);
+
+  const toggleSubMenu = () => {
+    setShowSubMenu(!showSubMenu);
+  };
   return (
     <div className='leftSidebar-section'>
       <div className="leftSidebar-container">
         <div className="left-navbar">
-          <a href="/"> <AiOutlineHome size={20} /> <p> Home </p> </a>
+          <a href="/"> <AiOutlineHome size={20} /> <p> Home </p></a>
+          <div className="menu-item" onClick={toggleSubMenu}>
+            <AiOutlineGlobal size={20} />
+            <p> Public </p>
+            {showSubMenu ? <AiOutlineUp /> : <AiOutlineDown />}
+          </div>
+          {showSubMenu && (
+              <div className="sub-menu">
+                <a href="/" id='active'> Questions </a>
+                <a href="/"> Tags </a>
+                <a href="/"> Users </a>
+              </div>
+            )}
           <a href="/"> <AiOutlineGlobal size={20} /> <p> Public </p> </a>
           <a href="/"> <AiOutlineTeam size={20} /> <p> Collectives </p> </a>
           <a href="/"> <HiOutlineBriefcase size={20} /> <p> Jobs </p> </a>
