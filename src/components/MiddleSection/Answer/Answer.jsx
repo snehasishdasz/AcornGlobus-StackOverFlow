d0c6fea5e900f2d2b41e371b871500c21c2c8a30
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2024-01-17 23:56:16 +0530

    Update the ANSWER part to dynamic

--- src/components/MiddleSection/Answer/Answer.jsx
+++ src/components/MiddleSection/Answer/Answer.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Answer.scss"
 import { FaMinus } from "react-icons/fa6";
 import { FiPlus } from "react-icons/fi";
+import parse from 'html-react-parser';
 
 const Answer = () => {
+  const [answerData, setAnswerData] = useState(null);
+  const [voteCount, setVoteCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchAnswer();
+  }, []); 
+
+  const fetchAnswer = async () => {
+    if (loading) return;
+
+    try {
+      setLoading(true);
+
+      const response = await fetch(
+        'https://api.stackexchange.com/2.3/answers?order=desc&sort=activity&site=stackoverflow&filter=!nNPvSNdWme'
+      );
+      const data = await response.json();
+
+      if (data.items && data.items.length > 0) {
+        setAnswerData(data.items[0]);
+        if (data.items[0].score) {
+          setVoteCount(data.items[0].score);
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleNextAnswer = () => {
+    fetchAnswer();
+  };
+
+
+
   return (
     <div className='answers-section'>
       <div className="answers-container">
@@ -20,9 +59,9 @@ const Answer = () => {
           <div className="auth-det">
             <div>
               <p> Author:  </p>
-              <p id='name'> javed (308) </p>
+              <p id='name'> {answerData.owner.display_name} </p>
             </div>
-            <p> Answered: Dec 9 '19 at 8:20 </p>
+            <p> Answered: {new Date(answerData.creation_date * 1000).toLocaleDateString()} </p>
           </div>
           <div className="btn">
             <p id='minus'> <FaMinus /> </p>
@@ -31,13 +70,11 @@ const Answer = () => {
           </div>
         </div>
 
-
-        <div className="answer-desc">
-          <p> For more simplicity, I want to add the vs code settings path in addition to Ricardo's answer. you can get it like this: </p>
-          <p> File -{">"} Preferences -{">"} Settings and in the search bar write "automation". </p>
-          <p> After that, by looking your operating system enter "edit in settings.json". </p>
-          <p> Finally, add the following b/n the braces:  </p>
+        {answerData && (
+          <div className="answer-desc">
+          <p>{parse(answerData.body)}</p>
         </div>
+        )}
       </div>
     </div>
   )
